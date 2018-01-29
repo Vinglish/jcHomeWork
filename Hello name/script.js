d2a526d51ee5b0898dbc3710af185c2f9e435ced
Vinglish <khan.ildar94@gmail.com>
2018-01-29 12:33:42 +0300

    My work

---
+++ Hello name/script.js
@@ -0,0 +1,4 @@
+var a = prompt("Enter your name:");
+document.write("Hello " + a);
+// console.log(a);
+// alert("Hello " + a);
---
+++ JavaScript/index.js
@@ -0,0 +1,18 @@
+var firstNumber;
+var secondNumber;
+
+firstNumber = Number(prompt("input first number"));
+secondNumber = Number(prompt("input second number"));
+
+var sum = firstNumber + secondNumber;
+var sub = firstNumber - secondNumber;
+var div = firstNumber / secondNumber;
+var mul = firstNumber * secondNumber;
+
+
+alert ("I am alert")
+
+document.write("sum is:" + sum + "<br>");
+document.write("sum is:" + sub +"<br />");
+document.write("sum is" + div + "<br />");
+document.write("sum is" + mul);
